4a204179db4e86d98a11e425184055b76cfdd4f2
zh32 <devnull@zh32.de>
2015-09-06 20:34:43 +0200

    Override default extension type

--- CraftlistCaptchaBundle.php
+++ CraftlistCaptchaBundle.php
@@ -2,7 +2,22 @@
 
 namespace Craftlist\Bundle\CaptchaBundle;
 
+use Craftlist\Bundle\CaptchaBundle\DependencyInjection\CraftlistCaptchaExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class CraftlistCaptchaBundle extends Bundle {
+    /**
+     * Returns the bundle's container extension.
+     *
+     * @return ExtensionInterface|null The container extension
+     *
+     * @throws \LogicException
+     *
+     * @api
+     */
+    public function getContainerExtension() {
+        return new CraftlistCaptchaExtension();
+    }
+
 }
